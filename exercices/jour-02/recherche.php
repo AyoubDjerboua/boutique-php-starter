4283ee90b4adf3880a1db4295532a3099635944c
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-06 15:12:53 +0100

    Allez go Exo 4

---
+++ exercices/jour-02/recherche.php
@@ -0,0 +1,12 @@
+<?php
+$categories = ["Vêtements", "Chaussures", "Accessoires", "Sport"];
+
+if (in_array("Chaussures", $categories))
+{
+    echo "Chaussures : Trouvé ! <br>";
+}
+else
+{
+    echo "Chaussures : Non trouvé<br>";
+}
+?>
