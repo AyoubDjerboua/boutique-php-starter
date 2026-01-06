228ba78a3e9b74697e47e5d0e17c10f4197f450b
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-06 16:38:33 +0100

    Allez go jour 3

---
+++ exercices/jour-02/fusion.php
@@ -0,0 +1,16 @@
+<?php 
+
+$clothes = ["T-shirt", "Jean", "Pull"];
+$accessories = ["Ceinture", "Montre", "Lunettes"];
+
+//fusion tablo
+$catalog = array_merge($clothes, $accessories);
+
+//afffichaientqz=jhdqznh total produits
+echo "Nbr de produits : " . count($catalog);
+
+//ajouter produit début tableau
+array_unshift($catalog, "Cartouche");
+
+echo "<br>";
+var_dump($catalog);
