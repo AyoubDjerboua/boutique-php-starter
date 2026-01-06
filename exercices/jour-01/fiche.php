f027f885cfeeab76ac64a43d7b9d71a9268e6a24
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-06 09:55:31 +0100

    Exo 6 finito

--- exercices/jour-01/fiche.php
+++ exercices/jour-01/fiche.php
@@ -1,5 +1,5 @@
 <?php
-$name = "Pomme";
+$name = "Pommes";
 $price = 149.99;
 $stock = 3000;
 ?>
@@ -8,9 +8,11 @@ $stock = 3000;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pommes</title>
+    <title><?php echo $name ?></title>
 </head>
 <body>
-    
+    <h1>Boutique de <?php echo $name ?><h1>
+        <p>Prix à l'unité : <?php echo $price?>€</p>
+        <span><?= $stock > 0 ? "En stock" : "Rupture" ?></span> 
 </body>
 </html>
