1e12c1d119fa58df11ea08abdae01d6f8e70ff84
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-07 10:13:29 +0100

    Ici ça mémo

--- exercices/jour-02/catalogue.php
+++ exercices/jour-02/catalogue.php
@@ -1,3 +1,11 @@
 <?php
 
-$products = [""]
+$products = [
+    ["name" => "Paquito Orange", "price" => 4.99, "stock" => 8654],
+    ["name" => "Oasis Pomme", "price" => 15.99, "stock" => 24],
+    ["name" => "Fuze Tea Citron", "price" => 29.99, "stock" => 45],
+    ["name" => "Sans Plomb 95", "price" => 349.99, "stock" => 1],
+    ["name" => "Nicolas Maduro", "price" => 3500.0, "stock" => 2]
+];
+
+var_dump($products);
