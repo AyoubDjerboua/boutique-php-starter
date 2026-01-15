ba25b8c0d407121e9bc05bb86bf31efb8af7f166
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-15 16:38:15 +0100

    Exo 2 terminé

--- exercices/jour-08/User.php
+++ exercices/jour-08/User.php
@@ -2,5 +2,37 @@
 
 class User
 {
-    
+
+    // Propriétés (les données)
+    public string $name;
+    public string $email;
+    public string $registrationDate;
+
+    // Constructeur
+    public function __construct(string $name, string $email, ?string $registrationDate = null)
+    {
+        $this->name = $name;
+        $this->email = $email;
+
+        if ($registrationDate === null) {
+            $this->registrationDate = date("Y-m-d");
+        } else {
+            $this->registrationDate = $registrationDate;
+        }
+    }
+
+    public function isNewMember(): bool
+    {
+        $registrationTimestamp = strtotime($this->registrationDate);
+        $currentTimestamp = time();
+        $daysSinceRegistration = ($currentTimestamp - $registrationTimestamp) / (60 * 60 * 24);
+
+        return $daysSinceRegistration < 30;
+    }
 }
+
+$user1 = new User("Alice", "alice@email.com");
+$user2 = new User("Bob", "bob@email.com", "2025-12-20");
+
+echo $user1->name . " est nouveau ? " . ($user1->isNewMember() ? "Oui" : "Non") . "<br>";
+echo $user2->name . " est nouveau ? " . ($user2->isNewMember() ? "Oui" : "Non") . "<br>";
