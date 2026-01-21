70a88342ca5e94e24df5e6090ea198e0350457f3
AyoubDjerboua <ayoub.djerboua26@gmail.com>
2026-01-21 11:59:38 +0100

    C'est bon flemme on passe au jour 10, menfout c'est validé ça marche flemme de rajouter 50 Usern leur date de naissance, la couleur de leur pyjama et lier le tout avec les phases de la lune, c'est bon

--- exercices/jour-09/Cart.php
+++ exercices/jour-09/Cart.php
@@ -2,46 +2,66 @@
 class Cart
 {
     /** @var CartItem[] */
-    private array $items = [];
-    
+
+
+    private array $items = []; //tableau vide qui contiendra les produits du panier
+
+    //AFFICHAGE DES ITEMS DU TABLEAU
+    public function getItems(): array //permet de récupérer les items du panier
+    {
+        return $this->items; //retourne le tableau items
+    }
+
+    /**
+     * Ajoute un produit (objet de la classe Product) au panier
+     * Si le produit existe déjà, augmente sa quantité
+     */
     public function addProduct(Product $product, int $quantity = 1): void
     {
-        $id = $product->getId();
-        
-        if (isset($this->items[$id])) {
-            // Produit déjà dans le panier → augmenter quantité
-            $currentQuantity = $this->items[$id]->getQuantity();
-            $this->items[$id]->setQuantity($currentQuantity + $quantity);
+        $id = $product->getId(); // Récupère l'ID unique du produit (méthode de la classe Product)
+
+        if (isset($this->items[$id])) { // IF Produit déjà dans le panier
+            $currentQuantity = $this->items[$id]->getQuantity(); // Chope la quantité via CartItem
+            $this->items[$id]->setQuantity($currentQuantity + $quantity); // Augmenter la quantité
         } else {
             // Nouveau produit
             $this->items[$id] = new CartItem($product, $quantity);
         }
     }
-    
+
+    /**Remove un produit du panier à partir de son ID */
     public function removeProduct(int $productId): void
     {
         unset($this->items[$productId]);
     }
-    
-    public function getItems(): array
+
+    /** Met à jour la quantité d'un produit déjà présent dans le panier */
+    public function update(int $productId, int $quantity): void
     {
-        return $this->items;
+        if (isset($this->items[$productId])) //IF produit déjà présent dans le panier
+        {
+            $this->items[$productId]->setQuantity($quantity); //on set la quantity
+        }
     }
-    
+
+    /** Calcule le total du panier (prix x quantité pour chaque CartItem) */
     public function getTotal(): float
     {
         $total = 0;
-        foreach ($this->items as $item) {
+        foreach ($this->items as $item) //Parcourt chaque CartItem du panier
+        {
             $total += $item->getTotal();
         }
         return $total;
     }
-    
+
+    /** Retourne le nombre de produits différents dans le panier */
     public function count(): int
     {
         return count($this->items);
     }
-    
+
+    /** Clear le panier */
     public function clear(): void
     {
         $this->items = [];
--- exercices/jour-09/ExoDELUXE.php
+++ exercices/jour-09/ExoDELUXE.php
@@ -15,62 +15,97 @@ $chaussures = new Category(547, "Chaussures"); //catégorie chaussures, avec son
 $chapeaux = new Category(12, "Chapeaux"); //catégorie chapeaux, avec son ID de catégorie et le nom de la catégorie
 
 //PRODUITS
-$tshirt = new Product("T-shirt Supreme", 29.99, $clothes); //le produit, son nom, son prix et sa catégorie
-$bonnet = new Product("Bonnet Lacoste", 39.99, $chapeaux); //pareil
-$débardeur = new Product("Débardeur", 9.99, $clothes); //la même
-$AirMax = new Product("Nike AirMax", 119.99, $chaussures); //allez
-$AirJordan = new Product("Air Jordan 1", 169.99, $chaussures); //bon abuse
+$tshirt = new Product(1, "T-shirt Supreme", 29.99, $clothes); //le new produit de class Product, son nom, son prix et sa catégorie
+$bonnet = new Product(2, "Bonnet Lacoste", 39.99, $chapeaux); //pareil
+$débardeur = new Product(3, "Débardeur", 9.99, $clothes); //la même
+$airMax = new Product(4, "Nike airMax", 119.99, $chaussures); //allez
+$airJordan = new Product(5, "Air Jordan 1", 169.99, $chaussures); //bon abuse
 
 //AFFICHAGE
-echo "Produit : " . $tshirt->getName() . ". <br> Catégorie : " . $tshirt->getCategory()->getName() . "<br><br>"; //echo le nom de la catégorie du tshirt, donc "Vêtements"
-echo "Produit : " . $bonnet->getName() . ". <br> Catégorie : " . $bonnet->getCategory()->getName() . "<br><br>";
-echo "Produit : " . $débardeur->getName() . ". <br> Catégorie : " . $débardeur->getCategory()->getName() . "<br><br>";
-echo "Produit : " . $AirMax->getName() . ". <br> Catégorie : " . $AirMax->getCategory()->getName() . "<br><br>";
-echo "Produit : " . $AirJordan->getName() . ". <br> Catégorie : " . $AirJordan->getCategory()->getName() . "<br><br>";
+echo "Produit : " . $tshirt->getName() . "<br> Catégorie : " . $tshirt->getCategory()->getName() . "<br><br>"; //echo le nom de la catégorie du tshirt, donc "Vêtements"
+echo "Produit : " . $bonnet->getName() . "<br> Catégorie : " . $bonnet->getCategory()->getName() . "<br><br>";
+echo "Produit : " . $débardeur->getName() . "<br> Catégorie : " . $débardeur->getCategory()->getName() . "<br><br>";
+echo "Produit : " . $airMax->getName() . "<br> Catégorie : " . $airMax->getCategory()->getName() . "<br><br>";
+echo "Produit : " . $airJordan->getName() . "<br> Catégorie : " . $airJordan->getCategory()->getName() . "<br><br>";
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------//
 
 //EXO 2
 
-$item = new CartItem($AirMax);  // quantité par défaut = 1
+$objet = new CartItem($airMax);  // quantité par défaut = 1
 
 //AFFICHAGE INITIAL
-echo "CART : " . $item->getProduct()->getName() . "<br>"; //chope le nom du produit
-echo "Quantité initiale : " . $item->getQuantity() . "<br>"; //chope la quantité initiale
-echo "Total : " . $item->getTotal() . "€ <br>";
+echo "CART : " . $objet->getProduct()->getName() . "<br>"; //chope le nom du produit
+echo "Quantité initiale : " . $objet->getQuantity() . "<br>"; //chope la quantité initiale
+echo "Total : " . $objet->getTotal() . "€ <br>";
 
 echo "<br><hr><hr><hr><br>";
 
 //SET QUANTITY
 echo "SET QUANTITY <br>";
-$item->setQuantity(200); //quantité fixée à 200
-echo "Quantité fixée à " . $item->getQuantity() . "<br>"; //affichage du stock
-echo "Prix total : " . number_format($item->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
+$objet->setQuantity(200); //quantité fixée à 200
+echo "Quantité fixée à " . $objet->getQuantity() . "<br>"; //affichage du stock
+echo "Prix total : " . number_format($objet->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
 
 echo "<br><hr><br>";
 
 //AFFICHAGE INCREMENTATION (+1 en gros)
 echo "INCREMENTATION <br>";
-$item->increment(); //+1
-echo "Produit incrémenté. Nouveau stock : " . $item->getQuantity() . "<br>"; //affichage du stock
-echo "Nouveau prix total : " . number_format($item->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
+$objet->increment(); //+1
+echo "Produit incrémenté. Nouveau stock : " . $objet->getQuantity() . "<br>"; //affichage du stock
+echo "Nouveau prix total : " . number_format($objet->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
 
 echo "<br><hr><br>";
 
 //AFFICHAGE DECREMENTATION (-1 en gros)
 echo "DECREMENTATION <br>";
-$item->decrement();
-echo "Produit décrémenté. Nouveau stock : " . $item->getQuantity() . "<br>"; //affichage du stock
-echo "Nouveau prix total : " . number_format($item->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
+$objet->decrement();
+echo "Produit décrémenté. Nouveau stock : " . $objet->getQuantity() . "<br>"; //affichage du stock
+echo "Nouveau prix total : " . number_format($objet->getTotal(), 2, ',', ' ') . "€ <br>"; //affichage du prix du stock total
 
 echo "<br><hr><br>";
 
 //AFFICHAGE TOTAL
 echo "AFFICHAGE PRIX TOTAL <br>";
-echo "Total : " . number_format($item->getTotal(), 2, ',', ' ') . "€ <br>";
+echo "Total : " . number_format($objet->getTotal(), 2, ',', ' ') . "€ <br>";
 
 echo "<br><hr><hr><hr><br>";
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------//
 
 //EXO 3
+
+$cart = new Cart();
+
+//AJOUT DANS LE PANIER
+$cart->addProduct($tshirt, 652);
+$cart->addProduct($bonnet, 4);
+$cart->addProduct($airJordan, 165);
+
+//AFFICHAGE DU PANIER
+echo "<h2>Contenu du panier</h2>";
+
+foreach ($cart->getItems() as $item) {
+    echo "Produit : " . $item->getProduct()->getName() . "<br>";
+    echo "Catégorie : " . $item->getProduct()->getCategory()->getName() . "<br>";
+    echo "Quantité : " . $item->getQuantity() . "<br>";
+    echo "Total : " . number_format($item->getTotal(), 2, ',', ' ') . "€ <br><br>";
+}
+echo "Total général du panier : " . number_format($cart->getTotal(), 2, ',', ' ') . " €<br>";
+echo "Nombre de produits : " . $cart->count();
+
+
+echo "<br><hr><br>";
+
+
+//UPDATE DU PANIER
+echo "<h2>UPDATE</H2>";
+
+$cart->update($tshirt->getId(), 5); //passe le T-shirt à 5 exemplaires
+
+foreach ($cart->getItems() as $item) {
+    echo $item->getProduct()->getName() . " → " . $item->getQuantity() . " pcs<br>";
+}
+
+echo "Nouveau total : " . number_format($cart->getTotal(), 2, ',', ' ') . " €<br>";
+echo "<hr>";
